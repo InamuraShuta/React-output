60f0a2c52262ae0612bf42f8453f2fe9747e9633
InamuraShuta <shuta_inamura@unirita.co.jp>
2026-04-01 00:57:29 +0000

    Cognit連携

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Authenticator } from '@aws-amplify/ui-react'
 import Outputmain from './Outputmain.tsx'
+
+import '@aws-amplify/ui-react/styles.css'
+
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+
+
+Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Outputmain />
+    <Authenticator.Provider> {/* Providerで囲むのがコツです */}
+      <Authenticator>
+        <Outputmain /> {/* Propsを渡さず、ただ呼び出すだけ */}
+      </Authenticator>
+    </Authenticator.Provider>
   </StrictMode>,
 )
